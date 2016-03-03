0c0ecb4dc5f0653cd12dbca7caef290001fc767f
Greg Allen <me@jga.me>
2016-03-03 12:45:52 -0800

    more refactor

--- function/index.js
+++ function/index.js
@@ -1,17 +1,18 @@
 'use strict';
 
-const repo = require('../lib/template-repository.js');
-const email = require('../lib/email.js');
+const TemplateRepository = require('../lib/template-repository.js');
+const Email = require('../lib/email.js');
 const logr = require('logr');
 const async = require('async');
 const log = require('logr')({
   type: 'json'
-});;
+});
 
-const GlobalTemplateCache = {};
+let GlobalTemplateCache = {};
 
-module.exports.handler = function(event, context) {
+module.exports.handler = (event, context) => {
   const config = {
+    debug: process.env.DEBUG,
     smtp: {
       host: process.env.SMTP_HOST,
       user: process.env.SMTP_USER,
@@ -23,24 +24,27 @@ module.exports.handler = function(event, context) {
     }
   };
 
-  if (event.refreshCache == 'refresh'){
+  if (event.refreshCache === 'refresh') {
     log(['refresh'], 'Cache Refreshed');
     GlobalTemplateCache = {};
     return context.succeed();
   }
 
+  const repo = new TemplateRepository(config.s3, GlobalTemplateCache);
+  const email = new Email(config.smtp, config.debug);
+
   async.auto({
     render: (done) => {
-      repo.renderTemplate(config, event, GlobalTemplateCache, done);
+      repo.renderTemplate(event, done);
     },
     send: ['render', (done, results) => {
-      email.sendOneEmail(config, results.render, done);
-    }
+      email.sendOneEmail(results.render, done);
+    }]
   }, (err, results) => {
-    if (err){
+    if (err) {
       logr(['error'], err);
       return context.fail(err);
     }
-    context.succeed(result);
+    context.succeed(results);
   });
-}
+};
